40921ea455115b32c5e21cf75b733d7d0c9c96ac
Aarav Sareen <96787824+arvsrn@users.noreply.github.com>
2022-11-26 22:42:18 +0530

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,218 +1,75 @@
-<script lang="ts">
-    import Project from "./Project.svelte";
-    import AboutProject from "./AboutProject.svelte";
-    import { fade } from 'svelte/transition';
-    import Blanket from "./Blanket.svelte";
-
-    let visible: boolean = false;
-    let duration: number = 100;
-    let show: boolean = false;
-
-    setTimeout(() => visible = true, 1);
-
-    const projects = [
-        {
-            name: "meshgradient",
-            shortDescription: "Built front-end with SvelteKit and hosted on Vercel.",
-            websiteLink: "https://meshgradient.vercel.app",
-            githubLink: "https://www.github.com/arvsrn/mesh",
-            paragraphs: [
-                { heading: 'About', text: 'Meshgradient is a tool for making beautiful mesh gradients. Unlike most mesh gradient generators, this app can export your gradient as SVG, and supports customisations like blob size and blending modes.' },
-            ],
-            color: "#BD8B60"
-        },
-    ];
-
-    let projectI: number = 0;
-</script>
-
-{#if visible}
 <main>
-    <main>
-        <h1 in:fade={{duration: duration * 2}}>Aarav<br>Sareen</h1>
-        <h2 in:fade={{duration: duration * 2}}>Full stack developer and designer. <br> <span>(I'm not a good designer)</span></h2>
-        <p in:fade={{duration: duration * 8}}>Aspiring software engineer and entrepreneur. I do designing (figma) and full-stack development with SvelteKit & Typescript. </p>
-        <a in:fade={{duration: duration * 8}} class="writing" href="/writing">See writing</a>
-        
-        <h3 in:fade={{duration: duration * 18}}>Projects</h3>
-        <div in:fade={{duration: duration * 18}}>
-            {#each projects as project, i}
-                <Project color={project.color} onclick={() => {projectI = i; show = true; window.scrollTo(0, 0)}} data={{name: project.name, description: project.shortDescription, github: project.githubLink}} />
-            {/each}
-        </div>
-    </main>
+    <h1>Work in progress...</h1>
+    <p>Aarav Sareen — Web developer & designer.</p>
+    <p>Working on some stuff right now, new site will be live in a few weeks.</p>
+    <h2>Contact</h2>
+    <p><a href="">twitter/@arvsrn</a> or <a href="">contact@aarv.me</a>.</p>
 </main>
-{/if}
-
-{#if show}
-    <Blanket onclick={() => show = false}>
-        <AboutProject 
-            name={projects[projectI].name}
-            shortDescription={projects[projectI].shortDescription}
-            websiteLink={projects[projectI].websiteLink}
-            githubLink={projects[projectI].githubLink}
-            paragraphs={projects[projectI].paragraphs}
-            color={projects[projectI].color}
-        ></AboutProject>
-    </Blanket>
-{/if}
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,400;0,600;1,600&family=Inter:wght@400;500;600;700;800;900&display=swap');
+
+    a {
+        display: inline;
+        width: fit-content;
+
+        color: #ccc;
+
+        text-decoration-color: #555 !important;
+        text-decoration-thickness: 2px !important;
+        text-decoration: underline;
+    }
+
     main {
+        position: absolute;
+        top: 0px; left: 0px;
+        
+        width: 100vw;
+        height: 100vh;
+
         display: flex;
         flex-direction: column;
-        align-items: center;
-
-        padding-top: 50px;
-    }
-
-    main > main {
-        display: flex;
-        flex-direction: column;
+        padding: 15vh 20vw;
         align-items: flex-start;
+        gap: 4px;
 
-        width: 600px;
-        gap: 17px;
-    }
-
-    main > main > * {
-        animation: transformY;
-        animation-iteration-count: 1;
-    }
-
-    @keyframes transformY {
-        0% {
-            transform: translateY(50px);
-        }
-        100% {
-            transform: translateY(0px);
-        }
-    }
-
-    main > main > div {
-        display: flex;
-        flex-direction: row;
-        gap: 22px;
+        background-color: #222222;
     }
 
     h1 {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 900;
-        font-size: 80px;
-        line-height: 87.02%;
+        font-family: 'IBM Plex Serif';
+        font-size: 22px;
+        font-weight: 600;
+        font-style: italic;
 
-        display: flex;
-        align-items: center;
-        letter-spacing: 0.01em;
+        color: #e5e5e5;
 
-        color: #FFFFFF;
+        margin-bottom: 8px;
     }
 
-    h2 {
-        font-family: 'Inter';
+    h1::before {
+        content: '☕';
         font-style: normal;
-        font-weight: 700;
-        font-size: 24px;
-        line-height: 29px;
-        display: flex;
-        align-items: center;
-        letter-spacing: 0.01em;
-
-        color: #A0A0A0;
-
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-    }
-
-    h2 > span {
-        color: #505050;
+        margin-right: 4px;
     }
 
     p {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 24px;
-
-        display: flex;
-        align-items: center;
-        letter-spacing: 0.01em;
-
-        color: #A0A0A0;
-
         width: 100%;
-        word-wrap: break-word;
-        padding-right: 80px;
+
+        font-family: Inter, sans-serif;
+        font-size: 15px;
+        font-weight: normal;
+
+        color: #aaa;
     }
 
-    a.writing {
-        font-family: 'IBM Plex Serif';
-        font-style: italic;
-        font-weight: 600;
-        font-size: 18px;
-        line-height: 24px;
-        
-        display: flex;
-        align-items: center;
-        letter-spacing: 0.01em;
+    h2 {
+        font-family: Inter, sans-serif;
+        font-size: 16px;
+        font-weight: bold;
 
-        color: #A0A0A0;
+        color: #ddd;
 
-        text-decoration: none;
-    }
-
-    a.writing::after {
-        content: '→';
-        transition: transform 0.07s ease-in;
-    }
-
-    a.writing:hover::after {
-        transform: translateX(5px);
-    }
-
-    h3 {
-        padding-top: 98px;
-
-        font-family: 'IBM Plex Mono';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-
-        display: flex;
-        align-items: center;
-        letter-spacing: 0.01em;
-
-        color: #978365;
-    }
-
-    @media only screen and (max-width: 710px) {
-        main > main {
-            width: 80vw;
-        }
-
-        h1 {
-            font-size: 11.5vw;
-        }
-
-        h2 {
-            font-size: clamp(20px, 3.9vw, 36px);
-            line-height: clamp(24px, 4.0vw, 36px);
-        }
-
-        p {
-            font-size: 1em;
-        }
-
-        main > div {
-            flex-wrap: wrap;
-        }
-
-        main {
-            overflow-x: hidden;
-        }
+        margin-top: 20px;
     }
 </style>
