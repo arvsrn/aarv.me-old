d1d13603e9afea03076f1a510b3123a82a17d8f7
Aarav Sareen <96787824+arvsrn@users.noreply.github.com>
2023-03-15 13:49:16 +0530

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<style>
+    :root {
+        --width: 500px;
+    }
+
+    @media only screen and (max-width: 595.24px) {
+        :root {
+            --width: 84vw;
+        }
+    }
+</style>
+
+<slot></slot>
